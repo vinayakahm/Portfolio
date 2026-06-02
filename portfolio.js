e7a3c86f701542ac6bc375fe8af1eb913199ad78
vinayakahm <vinayakpama.2000@gmail.com>
2026-06-02 11:31:22 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,37 @@
+const text = [
+    "Web Developer",
+    "SQL Developer",
+    "Java Developer",
+    "IT Professional"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+
+    if (count === text.length) {
+        count = 0;
+    }
+
+    currentText = text[count];
+
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById("typing").textContent = letter;
+
+    if (letter.length === currentText.length) {
+
+        count++;
+        index = 0;
+
+        setTimeout(type, 1200);
+
+    } else {
+
+        setTimeout(type, 100);
+    }
+
+})();
